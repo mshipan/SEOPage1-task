dd72a5ea959ada811e00edb673ba1cae9f2845c8
Shipan Mallik <mshipan657@gmail.com>
2024-01-13 22:54:10 +0600

    update design

--- src/components/ParentTaskCard.jsx
+++ src/components/ParentTaskCard.jsx
@@ -1,12 +1,16 @@
 import TaskCard from "./taskCard";
 
-const ParentTaskCard = () => {
+const ParentTaskCard = ({ taskBarTitile, taskBarColor }) => {
   return (
     <div className="bg-slate-200">
       <div className=" flex items-center justify-between p-5">
         <div className="flex items-center gap-1">
-          <div className="bg-red-600 w-6 h-6 rounded-s-full"></div>
-          <h1 className="font-notoSansJP text-black font-medium">Incomplete</h1>
+          {taskBarColor && (
+            <div className={` w-6 h-6 ${taskBarColor} rounded-s-full `}></div>
+          )}
+          <h1 className="font-notoSansJP text-black font-medium">
+            {taskBarTitile}
+          </h1>
         </div>
         <div className="bg-slate-300 w-6 h-6 flex items-center justify-center rounded-md">
           <p className="font-notoSansJP text-black ">0</p>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,13 +4,16 @@ const Home = () => {
   return (
     <div className="">
       <div className="w-[95vw] h-[85vh] overflow-x-scroll overflow-y-hidden scrollbar-thin scrollbar-thumb-rounded-full scrollbar-track-rounded-full scrollbar-thumb-blue-500 scrollbar-track-slate-300">
-        <div className="flex gap-2 w-[150vw]">
-          <ParentTaskCard />
-          <ParentTaskCard />
-          <ParentTaskCard />
-          <ParentTaskCard />
-          <ParentTaskCard />
-          <ParentTaskCard />
+        <div className="flex gap-2 w-[150vw] ">
+          <ParentTaskCard
+            taskBarTitile="Incomplete"
+            taskBarColor="bg-red-600"
+          />
+          <ParentTaskCard taskBarTitile="To Do" taskBarColor="bg-sky-500" />
+          <ParentTaskCard taskBarTitile="Doing" taskBarColor="bg-yellow-400" />
+          <ParentTaskCard taskBarTitile="Under Review" />
+          <ParentTaskCard taskBarTitile="Completed" />
+          <ParentTaskCard taskBarTitile="Overdelay" />
         </div>
       </div>
     </div>
